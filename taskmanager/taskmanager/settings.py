d74f29e245b9592f0db78006a2efd3587848781e
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-05 10:50:28 +0000

    "Added"

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -86,7 +86,9 @@ load_dotenv()
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
