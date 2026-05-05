83215f9310ed6792a01ffa1eb5afe4bf217d82ac
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-05 01:58:08 +0000

    "Changes saved"

--- taskmanager/core/admin.py
+++ taskmanager/core/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Project, Task
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Project)
-admin.site.register(Task)
+
+# 🔹 Custom User Admin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff')
+
+
+# 🔹 Project Admin
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_by')
+
+
+# 🔹 Task Admin
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'assigned_to')
