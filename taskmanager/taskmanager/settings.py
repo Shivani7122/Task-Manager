b4aaa767b0f75cf53ead0d0f4d0f5fdfab7dec97
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-05 01:13:08 +0000

    "Updated files"

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bn1kiulndslx7)=)cveoc*cbatm_*wfo)79gw9=8%=ghypvh_4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
