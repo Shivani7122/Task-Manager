83b8e1fa9251be9d0e5f12b752a92c5e0461741c
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-05-06 02:28:30 +0000

    "commit"

--- taskmanager/core/admin.py
+++ taskmanager/core/admin.py
@@ -1,21 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Project, Task
+
+from .models import (
+    User,
+    Project,
+    Task
+)
 
 
-# 🔹 Custom User Admin
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'role', 'is_staff')
+admin.site.register(User)
 
+admin.site.register(Project)
 
-# 🔹 Project Admin
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_by')
-
-
-# 🔹 Task Admin
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'status', 'assigned_to')
+admin.site.register(Task)
